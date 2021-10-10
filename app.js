d200d86719cd44356b1bd853f6555ee6ddf3dd51
Daniel Ileyko <rlejko13@gmail.com>
2021-10-10 12:29:33 +0300

    now everything is working, need to make the code more cleaner

--- app.js
+++ app.js
@@ -1,49 +1,82 @@
-
+let productsList = [];
 const { Telegraf } = require('telegraf');
+require('dotenv').config();
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-
 bot.command('start', ctx => {
     console.log(`username: ${ctx.from.username}`);
-    let greetingMessage = `hello there, choose option`;
+
+        const greetingMessage = `hello there, choose option`;
+        bot.telegram.sendMessage(ctx.chat.id, greetingMessage, {
+            reply_markup: {
+                inline_keyboard: [
+                    [{
+                        text: "create products list",
+                        callback_data: 'list'
+                    }],
+    
+                ]
+            }
+        })
+    
+});
+
+const hearsMessage = (ctx) => {
+    bot.on("message", ctx => {
+        if (ctx.message.text) {
+            const splittedText = ctx.message.text.split(' ');
+
+            if (splittedText.length <= 10) {
+                productsList.push(ctx.message.text);
+        
+                bot.telegram.sendMessage(ctx.chat.id, 'is that enough?', {
+                    reply_markup: {
+                        inline_keyboard: [
+                            [{
+                                text: "enough",
+                                callback_data: 'enough'
+                            }],
+            
+                        ]
+                    }
+                }) 
+            } else{
+                bot.telegram.sendMessage(ctx.chat.id, 'your text is too long, enter less text:')
+            }
+            
+        } else{
+            bot.telegram.sendMessage(ctx.chat.id, 'its not a text, please enter text, to create products list:')
+        }
+        
+    });
+}
+
+bot.action('list', (ctx) => {
+    const enterProductsMessage = `ok, ${ctx.from.username}, enter the products for list:`;
+    bot.telegram.sendMessage(ctx.chat.id, enterProductsMessage);
+    
+    hearsMessage(ctx)
+})
+
+
+bot.action('enough', async (ctx)=>{
+    await ctx.reply(`your products list is \n${productsList.join(', \n')}`).then(
+        productsList = []
+    )
+
+    const greetingMessage = `Wanna create new list?`;
     bot.telegram.sendMessage(ctx.chat.id, greetingMessage, {
         reply_markup: {
             inline_keyboard: [
                 [{
-                    text: "create product list",
+                    text: "create products list",
                     callback_data: 'list'
                 }],
 
             ]
         }
     })
-});
-
-let productsList = [];
-
-bot.action('list', ctx => {
-    let enterProductsMessage = `ok, ${ctx.from.username}, enter the products for list:`;
-    bot.telegram.sendMessage(ctx.chat.id, enterProductsMessage);
-    bot.hears(message => {
-        productsList.push(message);
-        bot.telegram.sendMessage(ctx.chat.id, 'is that enough?', {
-            reply_markup: {
-                inline_keyboard: [
-                    [{
-                        text: "enough",
-                        callback_data: 'enough'
-                    }],
-    
-                ]
-            }
-        })
-    });
-})
-
-
-bot.action('enough', (ctx)=>{
-    ctx.reply(productsList.join(', \n'))
 })
 
 bot.launch();
